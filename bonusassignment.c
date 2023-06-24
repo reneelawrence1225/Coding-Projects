dd97da69a96c34bde35e604324c8724185437a13
reneelawrence1225 <55823723+reneelawrence1225@users.noreply.github.com>
2023-06-24 00:06:52 -0400

    Update bonusassignment.c

--- bonusassignment.c
+++ bonusassignment.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+char * entirestring;
+void RecursivePermute(char * str[], int k){
+
+  if(k == 0) {
+  printf("%s\n", entirestring);
+    return;
+  }
+  else{
+    int r = 0;
+    while(r < k){
+
+    ExchangeCharacters(str[0], str[r]);
+    RecursivePermute(str+1, k - 1);
+      ExchangeCharacters(str[0], str[r]);
+      r++;
+    }
+    
+    
+  
+}
 
